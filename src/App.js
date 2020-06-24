a3ba7cdcf96a9fcd8a8eda71e8814f72a0315615
RebeccaRoach <rebeccaroach12@gmail.com>
2020-06-23 17:56:33 -0700

    Added addMovietoLibraryList as potential way to create a new movie with json

--- src/App.js
+++ src/App.js
@@ -3,30 +3,65 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import Nav from './components/Nav.js'
 import Home from './components/Home.js'
 import Customers from './components/Customers.js'
 import Library from './components/Library.js'
 import Search from './components/Search.js'
+import axios from 'axios';
 
 // we can consume the context anywhere if we export it like so:
 export const SessionContext = React.createContext();
 
-function App() {
+const App = () => {
 
   const [selectedCustomer, setSelectedCustomer] = useState(undefined);
   const [selectedMovie, setSelectedMovie] = useState(undefined);
+  const [library, setLibrary] = useState([]);
 
 
+  const addMovietoLibraryList = (moviejson, rails_id) => {
+    // updates library with new movie object
 
+    const libraryCopy = [...library];
+  
+    const newMovie = {
+      id: rails_id,
+      release_date: moviejson.release_date,
+      title: moviejson.title,
+      overview: moviejson.overview
+    }
+    
+    libraryCopy.push(newMovie);
+    setLibrary(libraryCopy);
+  }
+
+  const onAddMovie = (moviejson) => {
+    // passed down to search component
+    // make API call to post movie in Rails DB
+    // uses create action in Rails
+ 
+    const endpoint_uri = 'http://localhost:4000/movies'
+    
+    axios.post(endpoint_uri, moviejson)
+        // format the moviejson correctly for our rails api to recognize it
+      .then((response) => {
+        console.log(response);
+        // const id = response.data.movie.id;
+        // addMovietoLibraryList(moviejson, id)
+      })
+      .catch((error) => {
+        alert("There was an error adding a movie!")
+      });
+  }
 
   return (
     <SessionContext.Provider value={{
       selectedCustomer,
       selectedMovie,
+      library,
       setSelectedCustomer,
       setSelectedMovie}}>
       <Router>
@@ -35,7 +70,7 @@ function App() {
           <Switch>
             <Route path='/' exact component={Home} />
             <Route path='/library' component={Library} />
-            <Route path='/search' component={Search} />
+            <Route path='/search' component={Search} onAddMovie={onAddMovie} />
             <Route path='/customers' component={Customers} />
           </Switch>
         </div>
